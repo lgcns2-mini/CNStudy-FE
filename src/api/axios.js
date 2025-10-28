c9548eb1a0bfdfafd1f3d2e0183f7165988cc423
hyunsoojin <jhs7251@naver.com>
2025-10-28 13:02:19 +0900

    블로그 서비스 백엔드 포트변경

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
 export const http = axios.create({
-  baseURL: "http://172.31.4.104:8088",
+  baseURL: "http://172.31.4.104:8080",
   headers: { "Content-Type": "application/json" },
 });
 
